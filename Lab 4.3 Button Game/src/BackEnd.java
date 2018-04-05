15c65dc2931d6938834843622fc56288c4d60f5f
Author <PC@Dell>
2018-04-05 13:54:31 -0400

    idk

--- Lab 4.3 Button Game/src/BackEnd.java
+++ Lab 4.3 Button Game/src/BackEnd.java
@@ -7,36 +7,33 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class BackEnd 
 {
+
 	/*
 	 public static void main(String... args) {
-	        List<Score> books = readBooksFromCSV("books.txt");
+	        List<String> books = readHighScore("scores.txt");
 
-	        // let's print all the person read from CSV file
-	        for (Score b : books) {
+	        for (String b : books) {
 	            System.out.println(b);
 	        }
 	    }
-	    */
+	  */
 
-	    private static List<String> readHighScore(String fileName) {
+	    public static List<String> readHighScore(String fileName) {
 	        List<String> scores = new ArrayList<>();
 	        Path pathToFile = Paths.get(fileName);
 
-	        
-	        try (BufferedReader br = Files.newBufferedReader(pathToFile,
-	                StandardCharsets.US_ASCII)) {
+	   
+	        try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII)) {
 
-	           
 	            String line = br.readLine();
-
-	           
 	            while (line != null) {
-
-	            
-	                String[] attributes = line.split(",");
+	            String[] attributes = line.split(",");
 
 	               String score = createScore(attributes);
 	                scores.add(score);
@@ -55,11 +52,43 @@ public class BackEnd
 	        
 		return score;
 	}
+	 
+	   
+	class HighScore {
+		
+		private int score;
+		
+		public HighScore(int score) {
+			this.score=score;
+			
+		}
 
-	class Score{
-		   
-	   }
+		public int getScore() {
+			return score;
+		}
 
+		public void setScore(int score) {
+			this.score = score;
+		}
+		
+		public String toString() {
+	        return "High Score: " + score;
+	    }
 	}
+
+
+    public static void WritetoFile(int newHighScore) {
+
+        try {
+            FileWriter highScore = new FileWriter("scores.txt");
+            BufferedWriter bwr = new BufferedWriter(highScore);
+            bwr.write(newHighScore);
+            bwr.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+    }
+}
 
 
--- Lab 4.3 Button Game/src/GameCode.java
+++ Lab 4.3 Button Game/src/GameCode.java
@@ -1,8 +1,10 @@
 
+// import BackEnd.HighScore;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -13,10 +15,76 @@ import javafx.stage.Stage;
 public class GameCode extends Application
 {
 
-	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
+	private int score = 0;
+	private boolean scoring = false;
+	private int newHighScore = 0;
 	
+	
+    public static void main(String[] args) 
+    {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) 
+    {
+        primaryStage.setTitle("Click me!");
+        Button btn = new Button();
+        btn.setText("Click me");
+        Text txt = new Text(10,0, "Click Score");
+        Label highScore = new Label();
+        btn.setStyle("-fx-font: normal 12px 'monospace' ");
+        txt.setStyle("-fx-font: normal bold 15px 'monospace' "); 
+        highScore.setStyle("-fx-font: normal bold 15px 'monospace' "); 
+       
+        btn.setOnAction(new EventHandler<ActionEvent>() 
+        
+        {
+        	
+            @Override
+            public void handle(ActionEvent event) 
+            {
+            
+            	scoring = true;
+            	   score++;  
+            	   if(score> newHighScore) 
+            	   {
+            		   newHighScore = score;
+            		   BackEnd.WritetoFile((int) newHighScore);
+                   }
+            }
+            
+        });
+       
+        new AnimationTimer()
+        {
+        	
+        	public void handle(long now)
+        	{
+        		if(score!=0 && scoring)
+        		{
+        			 btn.setTranslateX((Math.random()*200)-100);
+        			 btn.setTranslateY((Math.random()*175)-75);
+        			 scoring = false;
+   
+        		}
+        	
+        		txt.setText("Score: " + Integer.toString(score));
+        		highScore.setText("High Score: " + Integer.toString(newHighScore));
+             }
+        }.start();
+    
+        
+   
+        
+       
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        root.getChildren().add(txt);
+        root.setAlignment(txt, Pos.TOP_LEFT);
+        root.setAlignment(highScore, Pos.TOP_RIGHT);
+        root.getChildren().add(highScore);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
 }
